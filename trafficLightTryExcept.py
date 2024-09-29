9839d726d4c861fb2c2f9d12d009e030158c2430
Raspberry A <albiolsafontalejandro@gmail.com>
2024-09-29 13:55:18 +0100

    initial commit

---
+++ trafficLightTryExcept.py
@@ -0,0 +1,53 @@
+import time;
+import RPi.GPIO as GPIO;
+
+redLight = 36
+yellowLight = 38
+greenLight = 40
+delay = 3
+
+def resetBoard():
+
+    turnOffLights()
+    GPIO.cleanup()
+
+def setOutMode(red, yellow, green):
+
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(red, GPIO.OUT)
+    GPIO.setup(yellow, GPIO.OUT)
+    GPIO.setup(green, GPIO.OUT)
+
+def turnOffLights():
+
+    GPIO.output(redLight, GPIO.LOW)
+    GPIO.output(yellowLight, GPIO.LOW)
+    GPIO.output(greenLight, GPIO.LOW)
+
+def turnOnAndOffLight(light, delay):
+
+    turnOffLights()
+    GPIO.output(light, GPIO.HIGH)
+    print(f"Turn on pin {light}")
+    time.sleep(delay)
+    GPIO.output(light, GPIO.LOW)
+    print(f"Turn off pin {light}")
+
+setOutMode(redLight, yellowLight, greenLight)
+
+try:
+    while True:
+
+        turnOnAndOffLight(redLight, delay)
+        turnOnAndOffLight(yellowLight, delay)
+        turnOnAndOffLight(greenLight, delay)
+
+        userInput = input("Type 'exit' to end the program").strip().lower()
+
+        if userInput == "exit":
+
+            break
+
+finally:
+
+    resetBoard()
